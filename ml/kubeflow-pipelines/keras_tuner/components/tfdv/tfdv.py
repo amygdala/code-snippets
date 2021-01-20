1a1cd109f363902cb77c21e35ec4bb359c6723f4
Amy Unruh <amy@infosleuth.net>
2021-01-20 08:47:35 -0800

    tfdv checkpoint

--- ml/kubeflow-pipelines/keras_tuner/components/tfdv/tfdv.py
+++ ml/kubeflow-pipelines/keras_tuner/components/tfdv/tfdv.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google Inc. All Rights Reserved.
+# Copyright 2021 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,24 +13,26 @@
 # limitations under the License.
 
 
-def generate_tfdv_stats(input_data: str, output_path: str, job_name: str, use_dataflow: bool,
+def generate_tfdv_stats(input_data: str, output_path: str, job_name: str, use_dataflow: str,
                         project_id: str, region:str, gcs_temp_location: str, gcs_staging_location: str,
                         whl_location: str = '', requirements_file: str = 'requirements.txt'):
-  import tensorflow_data_validation as tfdv
   import logging
   import time
 
-  import tensorflow_data_validation.statistics.stats_impl
   import tensorflow_data_validation as tfdv
+  import tensorflow_data_validation.statistics.stats_impl
   from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions, SetupOptions
 
   # pip download tensorflow_data_validation --no-deps --platform manylinux2010_x86_64 --only-binary=:all:
   # CHANGE this if your download resulted in a different filename.
 
+  logging.getLogger().setLevel(logging.INFO)
+  logging.info("Building pipeline options")
   # Create and set your PipelineOptions.
   options = PipelineOptions()
 
-  if use_dataflow:
+  if use_dataflow == 'true':
+    logging.info("using Dataflow")
     if not whl_location:
       logging.warning('tfdv whl file required with dataflow runner.')
       exit(1)
@@ -54,45 +56,7 @@ def generate_tfdv_stats(input_data: str, output_path: str, job_name: str, use_da
     pipeline_options=options)
 
 
-def main():
-
-  logging.getLogger().setLevel(logging.INFO)
-  parser = argparse.ArgumentParser(description='TVDV')
-
-  parser.add_argument(
-      '--project_id', default='aju-vtests2')
-  parser.add_argument(
-      '--region', default='us-central1')
-  parser.add_argument(
-      '--job_name', required=True)
-  parser.add_argument(
-      '--gcs-staging-location', required=True)
-  parser.add_argument(
-      '--gcs-temp-location', required=True)
-  parser.add_argument(
-      '--output-path', required=True)
-  parser.add_argument(
-      '--data-path', required=True)
-  # TFDV whl required for Dataflow runner. Download whl file with this command:
-  # pip download tensorflow_data_validation --no-deps --platform manylinux2010_x86_64 --only-binary=:all:
-  parser.add_argument('--whl-location')
-  parser.add_argument('--requirements_file', default='requirements.txt')
-  parser.add_argument('--use-dataflow', default=False, help='Run on Dataflow', action='store_true')
-  parser.add_argument('--local', dest='use-dataflow', help='Run locally', action='store_false')
-  args = parser.parse_args()
-
-  use_dataflow = False
-  if args.use_dataflow:
-    use_dataflow = True
-
-  generate_tfdv_stats(args.data_path, args.output_path, args.job_name, use_dataflow,
-      args.project_id, args.region, args.gcs_temp_location, args.gcs_staging_location,
-      args.whl_location, args.requirements_file)
-
-
-
 if __name__ == '__main__':
   import kfp
   kfp.components.func_to_container_op(generate_tfdv_stats,
-      output_component_file='../tfdv_component.yaml', base_image='gcr.io/aju-vtests2/tfdv-tests:v6')
-  # main()
+      output_component_file='../tfdv_component.yaml', base_image='gcr.io/aju-vtests2/tfdv-tests:v8')
